40fec76b293c5c056a8401a437d6550312ed9375
Syed Hassan Raza <shrhawk88@gmail.com>
2015-12-28 15:58:11 +0500

    Andy code review improvements

--- cms/static/js/spec/views/login_studio_spec.js
+++ cms/static/js/spec/views/login_studio_spec.js
@@ -5,34 +5,27 @@ function($, LoginFactory, AjaxHelpers, ViewUtils) {
         var submit_button;
 
         beforeEach(function() {
-            loadFixtures('login.underscore');
+            loadFixtures('mock/login.underscore');
             /*jshint unused: false*/
             var login_factory = new LoginFactory("/home/");
             submit_button = $('#submit');
         });
 
-        it('It will disable the submit button once it is clicked', function() {
+        it('disable the submit button once it is clicked', function() {
             spyOn(ViewUtils, 'redirect').andCallFake(function(){});
-            var addClassSpy = spyOn($.fn, 'addClass').andReturn(submit_button);
-            spyOn($.fn, 'removeClass').andReturn(submit_button);
             var requests = AjaxHelpers.requests(this);
+            expect(submit_button).not.toHaveClass('is-disabled');
             submit_button.click();
-            expect($.fn.addClass).toHaveBeenCalledWith('is-disabled');
-            expect(addClassSpy.mostRecentCall.object.selector).toEqual('#submit');
             AjaxHelpers.respondWithJson(requests, {'success': true});
-            expect($.fn.removeClass).not.toHaveBeenCalled();
+            expect(submit_button).toHaveClass('is-disabled');
         });
 
         it('It will not disable the submit button if there are errors in ajax request', function() {
             var requests = AjaxHelpers.requests(this);
-            var addClassSpy = spyOn($.fn, 'addClass').andReturn(submit_button);
-            var removeClassSpy = spyOn($.fn, 'removeClass').andReturn(submit_button);
+            expect(submit_button).not.toHaveClass('is-disabled');
             submit_button.click();
-            expect($.fn.addClass).toHaveBeenCalledWith('is-disabled');
-            expect(addClassSpy.mostRecentCall.object.selector).toEqual('#submit');
             AjaxHelpers.respondWithError(requests, {});
-            expect($.fn.removeClass).toHaveBeenCalledWith('is-disabled');
-            expect(removeClassSpy.mostRecentCall.object.selector).toEqual('#submit');
+            expect(submit_button).not.toHaveClass('is-disabled');
         });
     });
 });
